8a87896505de2940097308f15c6cf3067e2f10d9
mrm.dev <65317108+MijanurMim@users.noreply.github.com>
2025-05-08 11:01:17 +0600

    [DONE] All projects Completed

---
+++ project-4.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$units = 275;
+$bill = 0;
+
+
+if ($units <= 50) {
+    $bill = $units * 3.50;
+} elseif ($units <= 100) {
+    $bill = 50 * 3.50 + ($units - 50) * 4.00;
+} elseif ($units <= 200) {
+    $bill = 50 * 3.50 + 50 * 4.00 + ($units - 100) * 5.20;
+} else {
+    $bill = 50 * 3.50 + 50 * 4.00 + 100 * 5.20 + ($units - 200) * 6.50;
+}
+
+
+$bill = number_format($bill, 2);
+
+echo "Total Electricity Bill for {$units} units is BDT {$bill}";
+
+?>
